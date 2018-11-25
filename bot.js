516cb224e949b0afc87212a66340e4e807279abc
DevOfLua <45329766+DevOfLua@users.noreply.github.com>
2018-11-25 14:33:53 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+client.on('ready', function (evt) {
+    console.log("Bot has started");
+    console.log(client.commands);
+});
+
+fs.readdir('./Commands/',(err,files) => {
+    if(err) {
+        console.log(err)
+        return;
+    }
+
+    let isfiles = files.filter(f => f.split(".").pop() === "js");
+
+    if(isfiles.length <= 0) {
+        console.log("no command found")
+        return;
+    }
+    
+    console.log(`Loading ${isfiles.length} command!`)
+
+    isfiles.forEach((f,i) => {
+        let props = require(`./Commands/${f}`);
+        console.log(`${i + 1} ${f} loaded!`);
+        client.commands.set(props.help.name,props);
+    })
+})
+
+
+client.on('message', async message => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.member.roles.has('494358565549637642')) {
+        message.delete();
+        return;
+    }
+
+    let MessageArray = message.content.split(/\s+/g);
+    let command = MessageArray[0];
+    let args = MessageArray.slice(1);
+
+    if (command.startsWith(config.prefix)) {
+
+        let cmd = client.commands.get(command.slice(config.prefix.length));
+        
+        if (cmd) {
+           cmd.run(client,message,args);
+        }
+    }
+});
+
+client.login(config.token);
